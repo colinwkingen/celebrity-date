ddaba0994985aee69fcddbbd3e662f69798fa8a5
Colin W Kingen and Wolfgang Warneke <colin@>
2016-06-02 11:17:11 -0700

    add starting header and add question and answer arrays for each question

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var firstQuestion = ["Would you rather...","date an actress","or date a musician?"];
+var secondQuestionA = ["Do you prefer...","action movies","or sitcoms?"];
+var secondQuestionB = ["Are you more of a...","pop fan","or rock fan?"];
+var thirdQuestionAA = ["Do you like to watch...","kung fu movies","or superhero movies?"];
+var thirdQuestionAB = ["Are you a fan of","classic sitcoms","or offbeat sitcoms?"];
+var thirdQuestionBA = ["Do you listen to...","newer music","or older music?"];
+var thirdQuestionBB = ["Are you more of a...","Hippy","HESHER"];
